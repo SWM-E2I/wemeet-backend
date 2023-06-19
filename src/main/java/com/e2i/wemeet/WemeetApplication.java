57eb29b8774961c9739278564990069d48169f74
이기우 <kai@igiuui-MacBookPro.local>
2023-06-19 20:10:19 +0900

    init project

---
+++ src/main/java/com/e2i/wemeet/WemeetApplication.java
@@ -0,0 +1,13 @@
+package com.e2i.wemeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WemeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WemeetApplication.class, args);
+	}
+
+}
