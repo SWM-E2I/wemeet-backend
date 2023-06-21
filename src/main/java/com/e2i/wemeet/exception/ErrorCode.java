5f08c887bf88d420829f33acbf231aa2bd92cdd8
KAispread <kai@igiuui-MacBookPro.local>
2023-06-21 18:33:03 +0900

    feat(error): add exception (DataFormat, HttpRequest)

--- src/main/java/com/e2i/wemeet/exception/ErrorCode.java
+++ src/main/java/com/e2i/wemeet/exception/ErrorCode.java
@@ -4,6 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
+    HTTP_METHOD_NOT_ALLOWED(40000, "http.method.not.allowed"),
+
+    INVALID_DATA_FORMAT(40001, "invalid.data.format"),
+    INVALID_PHONE_FORMAT(40002, "invalid.phone.format"),
+    INVALID_EMAIL_FORMAT(40003, "invalid.email.format"),
+    INVALID_CREDENTIAL_FORMAT(40004, "invalid.credential.format"),
+
     INVALID_SMS_CREDENTIAL(40100, "invalid.sms.credential"),
 
     UNEXPECTED_INTERNAL(50000, "unexpected.internal")
