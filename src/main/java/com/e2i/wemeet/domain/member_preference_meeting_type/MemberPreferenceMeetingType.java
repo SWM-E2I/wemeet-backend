a2c4dbb43bd248028f0326b9e9f259f9fc18d22b
chaerim <qkrdbsk28@naver.com>
2023-06-21 17:35:03 +0900

    feat(memberPreferenceMeetingType): add memberPreferenceMeetingType entity

---
+++ src/main/java/com/e2i/wemeet/domain/member_preference_meeting_type/MemberPreferenceMeetingType.java
@@ -0,0 +1,42 @@
+package com.e2i.wemeet.domain.member_preference_meeting_type;
+
+import com.e2i.wemeet.domain.base.BaseTimeEntity;
+import com.e2i.wemeet.domain.code.Code;
+import com.e2i.wemeet.domain.member.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "MEMBER_PREFERENCE_MEETING_TYPE")
+@Getter
+@NoArgsConstructor
+public class MemberPreferenceMeetingType extends BaseTimeEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long MemberPreferenceMeetingTypeId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "memberId", nullable = false)
+  private Member member;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "groupCodeId", referencedColumnName = "groupCodeId")
+  @JoinColumn(name = "CodeId", referencedColumnName = "CodeId")
+  private Code code;
+
+  @Builder
+  public MemberPreferenceMeetingType(Long memberPreferenceMeetingTypeId, Member member, Code code) {
+    MemberPreferenceMeetingTypeId = memberPreferenceMeetingTypeId;
+    this.member = member;
+    this.code = code;
+  }
+}
