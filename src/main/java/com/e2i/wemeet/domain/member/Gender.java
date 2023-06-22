629ec6e7ffd0307558324d64a57b5fc88dda5d21
chaerim <qkrdbsk28@naver.com>
2023-06-23 02:52:33 +0900

    refactor(member): change return value from null to exception

--- src/main/java/com/e2i/wemeet/domain/member/Gender.java
+++ src/main/java/com/e2i/wemeet/domain/member/Gender.java
@@ -1,6 +1,9 @@
 package com.e2i.wemeet.domain.member;
 
+import com.e2i.wemeet.exception.ErrorCode;
+import com.e2i.wemeet.exception.badrequest.InvalidValueException;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.Arrays;
 
 public enum Gender {
 
@@ -10,12 +13,9 @@ public enum Gender {
   @JsonCreator
   public static Gender findBy(String value) {
 
-    for (Gender gender : Gender.values()) {
-      if (gender.name().equals(value.toUpperCase())) {
-        return gender;
-      }
-    }
-
-    return null;
+    return Arrays.stream(Gender.values())
+        .filter(gender -> gender.name().equals(value.toUpperCase()))
+        .findFirst()
+        .orElseThrow(() -> new InvalidValueException(ErrorCode.INVALID_GENDER_VALUE));
   }
 }
--- src/main/java/com/e2i/wemeet/exception/ErrorCode.java
+++ src/main/java/com/e2i/wemeet/exception/ErrorCode.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     INVALID_SMS_CREDENTIAL(40100, "invalid.sms.credential"),
+    INVALID_GENDER_VALUE(40200, "invalid.gender.value"),
 
     UNEXPECTED_INTERNAL(50000, "unexpected.internal")
     ;
