ef8f4ff2fa1a4eed6e21757fcb836f4e501bde8a
KAispread <kai@igiuui-MacBookPro.local>
2023-06-25 14:20:20 +0900

    feat(login): add sns credential number verification function

---
+++ src/main/java/com/e2i/wemeet/config/security/provider/SmsUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.e2i.wemeet.config.security.provider;
+
+import com.e2i.wemeet.config.security.model.MemberPrincipal;
+import com.e2i.wemeet.domain.member.Member;
+import com.e2i.wemeet.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class SmsUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    /*
+    * username == phone
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByPhoneNumber(username).orElse(null);
+
+        // SMS 인증을 요청한 사용자가 회원가입이 되어있지 않을 경우
+        if (member == null) {
+            return new MemberPrincipal();
+        }
+        return new MemberPrincipal(member);
+    }
+}
