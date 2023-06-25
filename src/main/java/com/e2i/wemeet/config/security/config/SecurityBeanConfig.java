3886266aa6331ab6db060c44293fb54385452580
KAispread <kai@igiuui-MacBookPro.local>
2023-06-25 14:38:58 +0900

    feat(jwt): configuration spring security filter chain and register bean

--- src/main/java/com/e2i/wemeet/config/security/config/SecurityBeanConfig.java
+++ src/main/java/com/e2i/wemeet/config/security/config/SecurityBeanConfig.java
@@ -1,43 +1,95 @@
 package com.e2i.wemeet.config.security.config;
 
 import com.e2i.wemeet.config.security.filter.AuthenticationExceptionFilter;
-import com.e2i.wemeet.config.security.filter.LoginAuthenticationFilter;
+import com.e2i.wemeet.config.security.filter.JwtAuthenticationFilter;
+import com.e2i.wemeet.config.security.filter.LoginProcessingFilter;
+import com.e2i.wemeet.config.security.filter.RefreshTokenProcessingFilter;
+import com.e2i.wemeet.config.security.handler.CustomAuthenticationSuccessHandler;
+import com.e2i.wemeet.config.security.provider.SmsCredentialAuthenticationProvider;
+import com.e2i.wemeet.config.security.provider.SmsUserDetailsService;
+import com.e2i.wemeet.config.security.token.TokenInjector;
+import com.e2i.wemeet.config.security.token.handler.AccessTokenHandler;
+import com.e2i.wemeet.config.security.token.handler.RefreshTokenHandler;
+import com.e2i.wemeet.domain.member.MemberRepository;
+import com.e2i.wemeet.service.credential.SmsCredentialService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class SecurityBeanConfig {
-    // 인증 과정에서 발생하는 예외를 처리
+
+    // 인증 과정에서 발생하는 예외를 처리하는 필터
     @Bean
-    public AuthenticationExceptionFilter authenticationExceptionFilter(ObjectMapper objectMapper, MessageSourceAccessor accessor) {
+    public AuthenticationExceptionFilter authenticationExceptionFilter(ObjectMapper objectMapper,
+        MessageSourceAccessor accessor) {
         return new AuthenticationExceptionFilter(objectMapper, accessor);
     }
 
     // 로그인 인증 처리 필터
     @Bean
-    public LoginAuthenticationFilter loginAuthenticationFilter(AuthenticationManager manager, ObjectMapper objectMapper) {
-        return new LoginAuthenticationFilter(manager, objectMapper);
+    public LoginProcessingFilter loginAuthenticationFilter(AuthenticationManager manager,
+        AuthenticationSuccessHandler successHandler, ObjectMapper objectMapper) {
+        return new LoginProcessingFilter(manager, successHandler, objectMapper);
+    }
+
+    // RefreshToken 재발급 요청 수행 필터
+    @Bean
+    public RefreshTokenProcessingFilter refreshTokenProcessingFilter(
+        RedisTemplate<String, String> redisTemplate, RefreshTokenHandler refreshTokenHandler,
+        TokenInjector tokenInjector, ObjectMapper objectMapper) {
+        return new RefreshTokenProcessingFilter(redisTemplate, refreshTokenHandler, tokenInjector,
+            objectMapper);
+    }
+
+    // AccessToken 유효성 검증 필터
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(AccessTokenHandler accessTokenHandler) {
+        return new JwtAuthenticationFilter(accessTokenHandler);
     }
 
     // Authentication Manager -> Provider 에 로그인 요청을 전달
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration,
+        AuthenticationProvider authenticationProvider) throws Exception {
         AuthenticationManager authenticationManager = authenticationConfiguration.getAuthenticationManager();
-        ProviderManager providerManager = new ProviderManager(new ArrayList<>(), authenticationManager);
 
-        return providerManager;
+        // Provider 등록
+        List<AuthenticationProvider> provider = new ArrayList<>();
+        provider.add(authenticationProvider);
+
+        return new ProviderManager(provider, authenticationManager);
     }
 
-    // 로그인 정보가 유효한지 확인 후 인증 객체 전달
-//    @Bean
-//    public AuthenticationProvider authenticationProvider() {
-//
-//    }
+    // Provider - 로그인 정보가 유효한지 확인 후 인증 객체 전달
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService,
+        SmsCredentialService credentialService) {
+        return new SmsCredentialAuthenticationProvider(userDetailsService, credentialService);
+    }
+
+    // UserDetailService - 유저가 입력한 값에 대한 인증 정보를 가져옴
+    @Bean
+    public UserDetailsService userDetailsService(MemberRepository memberRepository) {
+        return new SmsUserDetailsService(memberRepository);
+    }
+
+    // 사용자 로그인 요청 성공시 수행
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler(ObjectMapper objectMapper,
+        TokenInjector tokenInjector) {
+        return new CustomAuthenticationSuccessHandler(objectMapper, tokenInjector);
+    }
 }
--- src/main/java/com/e2i/wemeet/config/security/config/SecurityConfig.java
+++ src/main/java/com/e2i/wemeet/config/security/config/SecurityConfig.java
@@ -1,25 +1,31 @@
 package com.e2i.wemeet.config.security.config;
 
+import static org.springframework.http.HttpMethod.POST;
+
 import com.e2i.wemeet.config.security.filter.AuthenticationExceptionFilter;
-import com.e2i.wemeet.config.security.filter.LoginAuthenticationFilter;
+import com.e2i.wemeet.config.security.filter.JwtAuthenticationFilter;
+import com.e2i.wemeet.config.security.filter.LoginProcessingFilter;
+import com.e2i.wemeet.config.security.filter.RefreshTokenProcessingFilter;
+import com.e2i.wemeet.domain.member.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
-
-import static org.springframework.http.HttpMethod.POST;
 
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
 
     private final AuthenticationExceptionFilter authenticationExceptionFilter;
-    private final LoginAuthenticationFilter loginAuthenticationFilter;
-
+    private final LoginProcessingFilter loginProcessingFilter;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final RefreshTokenProcessingFilter refreshTokenProcessingFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -33,22 +39,42 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable);
 
+        // Spring Security 의 Session 기반 인증 처리 비활성화
+        http
+                .sessionManagement(
+                    sessionConfigurer -> sessionConfigurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        .sessionFixation().none()
+                        .disable());
+
+
         /* URL 인가 정책 적용
         * 허용 목록
         * - 회원 가입
         * - 인증 번호 발송, 인증 번호 확인
+        * 나머지 요청
+        * ->
          */
         http
                 .authorizeHttpRequests(
                         authorize -> authorize
-                                .requestMatchers(POST, "/v1/member", "/v1/auth/phone/*").permitAll()
-                                .anyRequest().authenticated()
-                );
+                                .requestMatchers(POST, "/v1/member", "/v1/auth/phone/**", "/v1/auth/refresh",
+                                    "/test**").permitAll()
+                                .requestMatchers("/v1/member/**").hasRole(Role.USER.name())
+                                .anyRequest().authenticated());
 
-        // 커스텀 필터 적용
+
+        /* 커스텀 필터 적용
+        * 1. AuthenticationExceptionFilter : Custom Filter 에서 발생하는 예외를 처리
+        * 2. LoginProcessingFilter : 사용자의 로그인 요청을 처리 (SMS 인증을 통해, Access, Refresh Token 발급)
+        * 3. RefreshTokenProcessingFilter : RefreshToken 재발급 요청 수행
+        * 4. JwtAuthenticationFiler : AccessToken 의 유효성을 검사 후, SecurityContext 에 인증 객체 할당
+         */
         http
                 .addFilterAfter(authenticationExceptionFilter, LogoutFilter.class)
-                .addFilterAfter(loginAuthenticationFilter, AuthenticationExceptionFilter.class);
+                .addFilterAfter(loginProcessingFilter, AuthenticationExceptionFilter.class)
+                .addFilterAfter(refreshTokenProcessingFilter, LoginProcessingFilter.class)
+                .addFilterBefore(jwtAuthenticationFilter, ExceptionTranslationFilter.class);
 
         return http.build();
     }
