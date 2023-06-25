d6dfaa54396c2e0352bae773f367992855d7115a
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2023-06-25 20:39:11 +0900

    feat(awsSns): add aws sns config  (#17)
    * build(gradle): add aws-sdk dependency
    
    * chore(git): add env.properties to .gitignore
    
    * feat(aws): add aws-sns message transfer service
    
    * style(checkstyle): add exception case for AbbreviationAsWordInName like AWSCofig
    
    * chore(gitignore): add EOF
    
    * test(awsSns): add AwsSnsService test
    
    * test(awsSns): add CredentialService test
    
    * test(awsSns): add failure case to AwsSnsServiceTest
    
    * test(awsSns): add EOF
    
    * chore(awsSns): change name from aws to awsSns
    
    * refactor: rename CredentialService to AwsCredentialService
    
    * build(gradle): add configuration processor dependency
    
    * refactor(awsSns): change properties used ConfigurationProperties
    
    * test(awsSns): change exception verification method from assertThrows to assertThatThrownBy
    
    * test(awsSns): change initialization method from setUp function to annotation

---
+++ src/main/java/com/e2i/wemeet/service/aws/AwsCredentialService.java
@@ -0,0 +1,30 @@
+package com.e2i.wemeet.service.aws;
+
+import com.e2i.wemeet.config.aws.AwsSnsConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.SnsClient;
+
+@RequiredArgsConstructor
+@Service
+public class AwsCredentialService {
+
+    private final AwsSnsConfig awsSnsConfig;
+
+    public AwsCredentialsProvider getAwsCredentials(String accessKeyId, String secretAccessKey) {
+        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKeyId,
+            secretAccessKey);
+        return () -> awsBasicCredentials;
+    }
+
+    public SnsClient getSnsClient() {
+        return SnsClient.builder()
+            .credentialsProvider(
+                getAwsCredentials(awsSnsConfig.getAccessKey(), awsSnsConfig.getSecretKey())
+            ).region(Region.of(awsSnsConfig.getRegion()))
+            .build();
+    }
+}
---
+++ src/main/java/com/e2i/wemeet/service/aws/AwsSnsService.java
@@ -0,0 +1,37 @@
+package com.e2i.wemeet.service.aws;
+
+import static com.e2i.wemeet.exception.ErrorCode.AWS_SNS_MESSAGE_TRANSFER_ERROR;
+
+import com.e2i.wemeet.exception.internal.InternalServerException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+import software.amazon.awssdk.services.sns.model.PublishResponse;
+import software.amazon.awssdk.services.sns.model.SnsException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class AwsSnsService {
+
+    private final AwsCredentialService awsCredentialService;
+
+    public void sendSms(String phoneNumber, String message) {
+        SnsClient snsClient = awsCredentialService.getSnsClient();
+        try {
+            PublishRequest request = PublishRequest.builder()
+                .message(message)
+                .phoneNumber(phoneNumber)
+                .build();
+
+            PublishResponse result = snsClient.publish(request);
+            log.info("Message sent. MessageId: " + result.messageId());
+
+        } catch (SnsException e) {
+            log.info(e.getMessage());
+            throw new InternalServerException(AWS_SNS_MESSAGE_TRANSFER_ERROR);
+        }
+    }
+}
