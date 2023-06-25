a8d5e8563d2b63f9ca3d07f0bd51d3b584f2f8f7
KAispread <kai@igiuui-MacBookPro.local>
2023-06-25 14:30:27 +0900

    test(jwt): test access token function

---
+++ src/test/java/com/e2i/wemeet/config/security/token/AccessTokenHandlerTest.java
@@ -0,0 +1,31 @@
+package com.e2i.wemeet.config.security.token;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.e2i.wemeet.config.security.token.handler.AccessTokenHandler;
+import com.e2i.wemeet.domain.member.Role;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessTokenHandlerTest {
+    @Autowired
+    private AccessTokenHandler accessTokenHandler;
+
+    @DisplayName("AccessToken 을 생성하고 생성한 토큰을 파싱하는데 성공한다.")
+    @Test
+    void tokenGenerateAndExtract() {
+        final Payload payload = new Payload(1L, Role.USER.name());
+
+        String token = accessTokenHandler.createToken(payload);
+        Payload extractedToken = accessTokenHandler.extractToken(token);
+
+        assertAll(
+            () -> assertThat(extractedToken.getMemberId()).isEqualTo(1L),
+            () -> assertThat(extractedToken.getRole()).isEqualTo(Role.USER.name())
+        );
+    }
+}
