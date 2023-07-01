015f800c3da4ed79e48abf5b3bfed261330ec26d
KAispread <kai@igiuui-MacBookPro.local>
2023-07-01 19:49:39 +0900

    feat(auth): add access token injection api for develop

---
+++ src/main/java/com/e2i/wemeet/controller/admin/TestTokenInjectionController.java
@@ -0,0 +1,59 @@
+package com.e2i.wemeet.controller.admin;
+
+import com.e2i.wemeet.config.security.token.Payload;
+import com.e2i.wemeet.config.security.token.TokenInjector;
+import com.e2i.wemeet.domain.member.Member;
+import com.e2i.wemeet.domain.member.MemberRepository;
+import com.e2i.wemeet.dto.response.ResponseDto;
+import com.e2i.wemeet.dto.response.ResponseStatus;
+import com.e2i.wemeet.exception.notfound.MemberNotFoundException;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/*
+* profile 이 prod 가 아닐 때만 동작하는 Controller
+* */
+@RequiredArgsConstructor
+@Transactional
+@Profile("!prod")
+@Controller
+public class TestTokenInjectionController {
+
+    private final TokenInjector tokenInjector;
+    private final MemberRepository memberRepository;
+
+    @PostMapping("/test/register")
+    public ResponseEntity<ResponseDto> registerAndReturnAccessToken(HttpServletResponse response, @RequestParam String fixture) {
+        Member request = AdminMemberFixture.getFixture(fixture).create();
+        Member member = memberRepository.save(request);
+
+        tokenInjector.injectAccessToken(response, new Payload(member.getMemberId(), member.getRole().name()));
+
+        return ResponseEntity.ok(
+            new ResponseDto(
+                ResponseStatus.SUCCESS,
+                "member register and access token injection success",
+                member)
+        );
+    }
+
+    // 특정 유저의
+    @PostMapping("/test/access/{memberId}")
+    public ResponseEntity<ResponseDto> getAccessToken(@PathVariable Long memberId, HttpServletResponse response) {
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(MemberNotFoundException::new);
+
+        tokenInjector.injectAccessToken(response, new Payload(member.getMemberId(), member.getRole().name()));
+
+        return ResponseEntity.ok(
+            new ResponseDto(ResponseStatus.SUCCESS, "Access Token Injection Success", null)
+        );
+    }
+}
