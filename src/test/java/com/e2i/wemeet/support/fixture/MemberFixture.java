527da483840fe508e153c993207fdf34077b56c8
KAispread <kai@igiuui-MacBookPro.local>
2023-07-02 18:18:28 +0900

    fix(test): add preference fixture for admin test

--- src/test/java/com/e2i/wemeet/support/fixture/MemberFixture.java
+++ src/test/java/com/e2i/wemeet/support/fixture/MemberFixture.java
@@ -1,16 +1,19 @@
 package com.e2i.wemeet.support.fixture;
 
 import static com.e2i.wemeet.support.fixture.CollegeInfoFixture.ANYANG_COLLEGE;
+import static com.e2i.wemeet.support.fixture.PreferenceFixture.GENERAL_PREFERENCE;
 
 import com.e2i.wemeet.domain.member.CollegeInfo;
 import com.e2i.wemeet.domain.member.Gender;
 import com.e2i.wemeet.domain.member.Mbti;
 import com.e2i.wemeet.domain.member.Member;
+import com.e2i.wemeet.domain.member.Preference;
 import com.e2i.wemeet.domain.member.Role;
 
 public enum MemberFixture {
-    KAI("4100", "kai", Gender.MALE, "01012341234", ANYANG_COLLEGE.create(), Mbti.INFJ,
-        "안녕하세요", 100, Role.USER)
+    KAI("4100", "kai", Gender.MALE, "01012341234",
+        ANYANG_COLLEGE.create(), GENERAL_PREFERENCE.create(),
+        Mbti.INFJ, "안녕하세요", 100, Role.USER)
     ;
 
     private final String memberCode;
@@ -18,18 +21,20 @@ public enum MemberFixture {
     private final Gender gender;
     private final String phoneNumber;
     private final CollegeInfo collegeInfo;
+    private final Preference preference;
     private final Mbti mbti;
     private final String introduction;
     private final int credit;
     private final Role role;
 
     MemberFixture(String memberCode, String nickname, Gender gender, String phoneNumber,
-        CollegeInfo collegeInfo, Mbti mbti, String introduction, int credit, Role role) {
+        CollegeInfo collegeInfo, Preference preference, Mbti mbti, String introduction, int credit, Role role) {
         this.memberCode = memberCode;
         this.nickname = nickname;
         this.gender = gender;
         this.phoneNumber = phoneNumber;
         this.collegeInfo = collegeInfo;
+        this.preference = preference;
         this.mbti = mbti;
         this.introduction = introduction;
         this.credit = credit;
