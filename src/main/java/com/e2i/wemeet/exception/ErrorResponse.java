a7acdf95c6febab5703b6b88b5e8879238da345d
chaerim <qkrdbsk28@naver.com>
2023-07-04 00:09:54 +0900

    refactor(errorResponse): add status data

--- src/main/java/com/e2i/wemeet/exception/ErrorResponse.java
+++ src/main/java/com/e2i/wemeet/exception/ErrorResponse.java
@@ -1,5 +1,8 @@
 package com.e2i.wemeet.exception;
 
-public record ErrorResponse(int code, String message) {
+public record ErrorResponse(String status, int code, String message) {
 
+    public ErrorResponse(int code, String message) {
+        this("ERROR", code, message);
+    }
 }
