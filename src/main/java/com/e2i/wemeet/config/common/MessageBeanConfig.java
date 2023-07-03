3ebab38a82da87b9e5de71e16cdf45c8860a57bc
chaerim <qkrdbsk28@naver.com>
2023-07-03 17:44:15 +0900

    chore(message): add validation message setting

--- src/main/java/com/e2i/wemeet/config/common/MessageBeanConfig.java
+++ src/main/java/com/e2i/wemeet/config/common/MessageBeanConfig.java
@@ -6,9 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MessageBeanConfig {
+public class MessageBeanConfig implements WebMvcConfigurer {
+
     @Bean
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
@@ -22,5 +26,12 @@ public class MessageBeanConfig {
     @Bean
     public MessageSourceAccessor messageSourceAccessor() {
         return new MessageSourceAccessor(messageSource());
+    }
+
+    @Override
+    public Validator getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
     }
 }
