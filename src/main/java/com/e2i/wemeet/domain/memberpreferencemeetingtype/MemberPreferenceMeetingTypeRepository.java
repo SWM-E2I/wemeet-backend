fec8801f61723e3df33995a6bc7eb065eca8f616
chaerim <qkrdbsk28@naver.com>
2023-07-07 00:41:57 +0900

    refactor(member): add persistence context code

--- src/main/java/com/e2i/wemeet/domain/memberpreferencemeetingtype/MemberPreferenceMeetingTypeRepository.java
+++ src/main/java/com/e2i/wemeet/domain/memberpreferencemeetingtype/MemberPreferenceMeetingTypeRepository.java
@@ -2,12 +2,14 @@ package com.e2i.wemeet.domain.memberpreferencemeetingtype;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 public interface MemberPreferenceMeetingTypeRepository extends
     JpaRepository<MemberPreferenceMeetingType, Long> {
 
     List<MemberPreferenceMeetingType> findByMemberMemberId(Long memberId);
 
+    @Modifying(clearAutomatically = true)
     void deleteAllByMemberMemberId(Long memberId);
 
 }
