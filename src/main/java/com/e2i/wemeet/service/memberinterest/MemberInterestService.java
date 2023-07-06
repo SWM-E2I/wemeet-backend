cc1b1c376d7c61e0cd73172654f510ba634f6658
chaerim <qkrdbsk28@naver.com>
2023-07-06 21:48:35 +0900

    refactor(member): delete unused service

--- src/main/java/com/e2i/wemeet/service/memberinterest/MemberInterestService.java
+++
@@ -1,9 +0,0 @@
-package com.e2i.wemeet.service.memberinterest;
-
-import com.e2i.wemeet.domain.memberinterest.MemberInterest;
-import java.util.List;
-
-public interface MemberInterestService {
-
-    List<MemberInterest> findMemberInterestByMemberId(Long memberId);
-}
