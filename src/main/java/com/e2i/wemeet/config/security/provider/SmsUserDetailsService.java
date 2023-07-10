7a46105e5e4d35032f787a50451bfd2e1c0107d4
KAispread <kai@igiuui-MacBookPro.local>
2023-07-11 02:00:33 +0900

    style(encrypt): change encryption logic location

--- src/main/java/com/e2i/wemeet/config/security/provider/SmsUserDetailsService.java
+++ src/main/java/com/e2i/wemeet/config/security/provider/SmsUserDetailsService.java
@@ -3,6 +3,7 @@ package com.e2i.wemeet.config.security.provider;
 import com.e2i.wemeet.config.security.model.MemberPrincipal;
 import com.e2i.wemeet.domain.member.Member;
 import com.e2i.wemeet.domain.member.MemberRepository;
+import com.e2i.wemeet.util.encryption.EncryptionUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,8 +18,10 @@ public class SmsUserDetailsService implements UserDetailsService {
      * username == phone
      */
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member member = memberRepository.findByPhoneNumber(username).orElse(null);
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+        String hashPhoneNumber = EncryptionUtils.hashData(username.replace("+82", "0"));
+
+        Member member = memberRepository.findByPhoneNumber(hashPhoneNumber).orElse(null);
 
         // SMS 인증을 요청한 사용자가 회원가입이 되어있지 않을 경우
         if (member == null) {
