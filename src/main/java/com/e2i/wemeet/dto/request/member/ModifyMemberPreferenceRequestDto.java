051bf73952f3878ce36bfd108e8c3d8db705535b
chaerim <qkrdbsk28@naver.com>
2023-07-10 17:24:54 +0900

    refactor(member): add builder to dto

--- src/main/java/com/e2i/wemeet/dto/request/member/ModifyMemberPreferenceRequestDto.java
+++ src/main/java/com/e2i/wemeet/dto/request/member/ModifyMemberPreferenceRequestDto.java
@@ -3,7 +3,9 @@ package com.e2i.wemeet.dto.request.member;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
+import lombok.Builder;
 
+@Builder
 public record ModifyMemberPreferenceRequestDto(
     @NotBlank(message = "{not.blank.drinking.option}")
     String drinkingOption,
--- src/main/java/com/e2i/wemeet/dto/request/member/ModifyMemberRequestDto.java
+++ src/main/java/com/e2i/wemeet/dto/request/member/ModifyMemberRequestDto.java
@@ -1,15 +1,16 @@
 package com.e2i.wemeet.dto.request.member;
 
-import com.e2i.wemeet.domain.member.Mbti;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
+import lombok.Builder;
 
+@Builder
 public record ModifyMemberRequestDto(
     @NotBlank(message = "{not.blank.nickname}")
     String nickname,
     @NotNull(message = "{not.null.mbti}")
-    Mbti mbti,
+    String mbti,
     @NotBlank(message = "{not.null.introduction}")
     String introduction,
 
