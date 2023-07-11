b80050c3a0a87063209735e3dc9adf9e23bdf882
chaerim <qkrdbsk28@naver.com>
2023-07-11 23:49:33 +0900

    feat(team): add TeamPreferenceMeetingType entity

---
+++ src/main/java/com/e2i/wemeet/domain/teampreferencemeetingtype/TeamPreferenceMeetingTypeRepository.java
@@ -0,0 +1,8 @@
+package com.e2i.wemeet.domain.teampreferencemeetingtype;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamPreferenceMeetingTypeRepository extends
+    JpaRepository<TeamPreferenceMeetingType, Long> {
+
+}
