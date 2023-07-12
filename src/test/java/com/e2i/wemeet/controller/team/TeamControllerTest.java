e1dd9b94ceba05dabafbb7c8dfff76034c171f94
chaerim <qkrdbsk28@naver.com>
2023-07-13 04:21:24 +0900

    test(team): add create-team test case

---
+++ src/test/java/com/e2i/wemeet/controller/team/TeamControllerTest.java
@@ -0,0 +1,104 @@
+package com.e2i.wemeet.controller.team;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.e2i.wemeet.dto.request.team.CreateTeamRequestDto;
+import com.e2i.wemeet.service.code.CodeService;
+import com.e2i.wemeet.service.team.TeamService;
+import com.e2i.wemeet.support.config.AbstractUnitTest;
+import com.e2i.wemeet.support.config.WithCustomMockUser;
+import com.e2i.wemeet.support.fixture.TeamFixture;
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.ResultActions;
+
+@WebMvcTest(TeamController.class)
+class TeamControllerTest extends AbstractUnitTest {
+
+    @MockBean
+    private TeamService teamService;
+
+    @MockBean
+    private CodeService codeService;
+
+
+    @DisplayName("팀 생성 성공")
+    @WithCustomMockUser
+    @Test
+    void createTeam_Success() throws Exception {
+        // given
+        CreateTeamRequestDto request = TeamFixture.TEST_TEAM.createTeamRequestDto();
+
+        when(codeService.findCodeList(anyList())).thenReturn(List.of());
+        when(teamService.createTeam(anyLong(), any(CreateTeamRequestDto.class), anyList()))
+            .thenReturn(1L);
+
+        // when
+        ResultActions perform = mockMvc.perform(post("/v1/team")
+            .with(csrf())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(toJson(request)));
+
+        perform
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.status").value("SUCCESS"))
+            .andExpect(jsonPath("$.message").value("Create Team Success"))
+            .andExpect(jsonPath("$.data").exists());
+
+        // then
+        verify(teamService).createTeam(1L, request, List.of());
+
+        createTeamWriteRestDocs(perform);
+    }
+
+    private void createTeamWriteRestDocs(ResultActions perform) throws Exception {
+        perform
+            .andDo(
+                MockMvcRestDocumentationWrapper.document("팀 생성",
+                    ResourceSnippetParameters.builder()
+                        .tag("팀 생성")
+                        .summary("팀 생성 API 입니다.")
+                        .description(
+                            """
+                                    팀 생성을 진행합니다.
+                                """),
+                    requestFields(
+                        fieldWithPath("memberCount").type(JsonFieldType.NUMBER).description("인원수"),
+                        fieldWithPath("region").type(JsonFieldType.STRING).description("선호 지역"),
+                        fieldWithPath("drinkingOption").type(JsonFieldType.STRING)
+                            .description("술자리 여부"),
+                        fieldWithPath("preferenceMeetingTypeList").type(JsonFieldType.ARRAY)
+                            .description("선호 미팅 유형"),
+                        fieldWithPath("additionalActivity").type(JsonFieldType.STRING).optional()
+                            .description("추가 활동"),
+                        fieldWithPath("introduction").type(JsonFieldType.STRING)
+                            .description("팀 소개")
+                    ),
+                    responseFields(
+                        fieldWithPath("status").type(JsonFieldType.STRING).description("응답 상태"),
+                        fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                        fieldWithPath("data").type(JsonFieldType.NUMBER)
+                            .description("생성된 팀의 teamId")
+                    )
+                ));
+    }
+
+}
---
+++ src/test/java/com/e2i/wemeet/support/fixture/TeamFixture.java
@@ -0,0 +1,71 @@
+package com.e2i.wemeet.support.fixture;
+
+import com.e2i.wemeet.domain.member.Gender;
+import com.e2i.wemeet.domain.member.Member;
+import com.e2i.wemeet.domain.team.AdditionalActivity;
+import com.e2i.wemeet.domain.team.Team;
+import com.e2i.wemeet.dto.request.team.CreateTeamRequestDto;
+import java.util.List;
+
+public enum TeamFixture {
+
+    TEST_TEAM("df42hg", 3, Gender.MALE, "건대입구", "0", AdditionalActivity.SHOW,
+        "안녕하세요. 저희 팀은 멋쟁이 팀입니다.", MemberFixture.KAI.create());
+
+    private final String teamCode;
+
+    private final int memberCount;
+
+    private final Gender gender;
+
+    private final String region;
+
+    private final String drinkingOption;
+
+    private final AdditionalActivity additionalActivity;
+
+    private final String introduction;
+
+    private final Member member;
+
+    TeamFixture(String teamCode, int memberCount, Gender gender, String region,
+        String drinkingOption, AdditionalActivity additionalActivity, String introduction,
+        Member member) {
+        this.teamCode = teamCode;
+        this.memberCount = memberCount;
+        this.gender = gender;
+        this.region = region;
+        this.drinkingOption = drinkingOption;
+        this.additionalActivity = additionalActivity;
+        this.introduction = introduction;
+        this.member = member;
+    }
+
+    public Team create() {
+        return createBuilder()
+            .build();
+    }
+
+    public CreateTeamRequestDto createTeamRequestDto() {
+        return CreateTeamRequestDto.builder()
+            .memberCount(this.memberCount)
+            .region(this.region)
+            .drinkingOption(this.drinkingOption)
+            .additionalActivity(this.additionalActivity.toString())
+            .preferenceMeetingTypeList(List.of("G001_C001"))
+            .introduction(this.introduction)
+            .build();
+    }
+
+    private Team.TeamBuilder createBuilder() {
+        return Team.builder()
+            .teamCode(this.teamCode)
+            .memberCount(this.memberCount)
+            .gender(this.gender)
+            .region(region)
+            .drinkingOption(this.drinkingOption)
+            .additionalActivity(this.additionalActivity)
+            .member(this.member)
+            .introduction(this.introduction);
+    }
+}
