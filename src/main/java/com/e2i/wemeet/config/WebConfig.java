cb945f3c1eea6a65e428f5a1316142334813efd9
KAispread <kai@igiuui-MacBookPro.local>
2023-07-27 22:21:53 +0900

    feat(resolver): add argument resolver getting memberId

--- src/main/java/com/e2i/wemeet/config/WebConfig.java
+++ src/main/java/com/e2i/wemeet/config/WebConfig.java
@@ -3,15 +3,23 @@ package com.e2i.wemeet.config;
 import com.e2i.wemeet.config.aws.AwsS3Config;
 import com.e2i.wemeet.config.aws.AwsSesConfig;
 import com.e2i.wemeet.config.aws.AwsSnsConfig;
+import com.e2i.wemeet.config.resolver.member.MemberIdArgumentResolver;
 import com.e2i.wemeet.util.encryption.AdvancedEncryptionStandard;
+import java.util.List;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableConfigurationProperties({
     AwsSesConfig.class, AwsSnsConfig.class, AwsS3Config.class,
     AdvancedEncryptionStandard.class
 })
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new MemberIdArgumentResolver());
+    }
 }
