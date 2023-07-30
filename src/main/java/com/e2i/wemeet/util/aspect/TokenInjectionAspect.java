44b216c1304d9ddb9354d1510ff85b2714d92739
chaerim <qkrdbsk28@naver.com>
2023-07-30 16:09:35 +0900

    refactor(token): use final object for token inject

--- src/main/java/com/e2i/wemeet/util/aspect/TokenInjectionAspect.java
+++ src/main/java/com/e2i/wemeet/util/aspect/TokenInjectionAspect.java
@@ -6,7 +6,6 @@ import com.e2i.wemeet.config.security.token.TokenInjector;
 import com.e2i.wemeet.domain.member.Role;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -23,8 +22,7 @@ public class TokenInjectionAspect {
         pointcut = "execution(* com.e2i.wemeet.service.member.MemberService.createMember(..))",
         returning = "memberId"
     )
-    public void injectTokenAdvice(JoinPoint joinPoint, Long memberId) {
-        HttpServletResponse response = getHttpServletResponseFromJoinPointArgs(joinPoint.getArgs());
+    public void injectTokenAdvice(Long memberId) {
         tokenInjector.injectToken(response, new MemberPrincipal(memberId, Role.USER.name()));
     }
 
@@ -42,12 +40,5 @@ public class TokenInjectionAspect {
     public void injectManagerTokenAdvice(final Long memberId) {
         Payload tokenPayload = new Payload(memberId, Role.MANAGER.name());
         tokenInjector.injectToken(response, tokenPayload);
-    }
-
-    private HttpServletResponse getHttpServletResponseFromJoinPointArgs(Object[] args) {
-        if (args.length >= 2 && args[1] instanceof HttpServletResponse response) {
-            return response;
-        }
-        return null;
     }
 }
