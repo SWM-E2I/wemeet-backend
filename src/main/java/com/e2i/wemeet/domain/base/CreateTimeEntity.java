8fc6f74827469ccf62b97c52bf34bd38b8ec029f
KAispread <kai@igiuui-MacBookPro.local>
2023-08-02 23:13:15 +0900

    refactor(base): separate createAt modifiedAt column

--- src/main/java/com/e2i/wemeet/domain/base/CreateTimeEntity.java
+++ src/main/java/com/e2i/wemeet/domain/base/CreateTimeEntity.java
@@ -5,15 +5,16 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
-import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseTimeEntity extends CreateTimeEntity {
+public abstract class CreateTimeEntity {
 
-    @LastModifiedDate
-    @Column(name = "MODIFIED_AT")
-    private LocalDateTime modifiedAt;
+    @CreatedDate
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+
 }
