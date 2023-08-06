37ddd048c8a0009a524cf885567cdeb8abbd9656
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2023-08-06 20:54:38 +0900

    feat(code): add initialize code sql on local envirionment (#87)
    * feat(codeFixture): add getter methods
    
    * feat(sql): add code insert sql to submodule
    
    * feat(code): add find code by code_value query & test
    
    * refactor(repository): change code setUp method in AbstractRepositoryUnitTest
    
    ---------
    
    Co-authored-by: KAispread <kai@igiuui-MacBookPro.local>

--- src/main/java/com/e2i/wemeet/domain/code/CodeRepository.java
+++ src/main/java/com/e2i/wemeet/domain/code/CodeRepository.java
@@ -1,7 +1,10 @@
 package com.e2i.wemeet.domain.code;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CodeRepository extends JpaRepository<Code, CodePk> {
+
+    Optional<Code> findByCodeValue(String codeValue);
 
 }
