6efe00187ecebf2d51f924240a6ed804383e72d5
chaerim <qkrdbsk28@naver.com>
2023-08-06 21:15:16 +0900

    feat(teamImage): add teamImage entity

---
+++ src/main/java/com/e2i/wemeet/domain/team_image/TeamImageRepository.java
@@ -0,0 +1,7 @@
+package com.e2i.wemeet.domain.team_image;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamImageRepository extends JpaRepository<TeamImage, Long> {
+
+}
