61ccf960290c57b6ecc83cb38d0872b05bc855c4
KAispread <kai@igiuui-MacBookPro.local>
2023-08-08 16:33:05 +0900

    feat(validator): add gender value validator

--- src/main/java/com/e2i/wemeet/dto/request/member/CreateMemberRequestDto.java
+++ src/main/java/com/e2i/wemeet/dto/request/member/CreateMemberRequestDto.java
@@ -5,11 +5,11 @@ import com.e2i.wemeet.domain.member.Member;
 import com.e2i.wemeet.domain.member.data.Gender;
 import com.e2i.wemeet.domain.member.data.Mbti;
 import com.e2i.wemeet.domain.member.data.Role;
+import com.e2i.wemeet.util.validator.bean.GenderValid;
 import com.e2i.wemeet.util.validator.bean.MbtiValid;
 import com.e2i.wemeet.util.validator.bean.NicknameValid;
 import com.e2i.wemeet.util.validator.bean.PhoneValid;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
@@ -17,7 +17,7 @@ public record CreateMemberRequestDto(
     @NicknameValid
     String nickname,
 
-    @NotBlank(message = "{not.blank.gender}")
+    @GenderValid
     String gender,
 
     @PhoneValid
