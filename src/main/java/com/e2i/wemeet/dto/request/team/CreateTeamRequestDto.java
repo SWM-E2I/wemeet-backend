66eb7cd2697bd91d9337100877f2573c1daa4351
chaerim <qkrdbsk28@naver.com>
2023-08-08 22:40:44 +0900

    refactor(team): modify CreateTeamRequestDto field

--- src/main/java/com/e2i/wemeet/dto/request/team/CreateTeamRequestDto.java
+++ src/main/java/com/e2i/wemeet/dto/request/team/CreateTeamRequestDto.java
@@ -9,7 +9,6 @@ import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.Builder;
 import org.hibernate.validator.constraints.Length;
-import org.springframework.web.multipart.MultipartFile;
 
 @Builder
 public record CreateTeamRequestDto(
@@ -20,23 +19,18 @@ public record CreateTeamRequestDto(
     @NotBlank(message = "{not.blank.region}")
     String region,
 
-    @Max(value = 100, message = "{max.validate.drink.rate}")
-    @Min(value = 0, message = "{min.validate.drink.rate}")
-    int drinkRate,
+    @NotBlank(message = "{not.blank.drink.rate}")
+    String drinkRate,
 
     @NotBlank(message = "{not.blank.drink.with.game}")
     String drinkWithGame,
 
     @Nullable
     String additionalActivity,
-    
+
     @Length(min = 1, max = 150, message = "{length.validate.introduction}")
     @NotBlank(message = "{not.blank.introduction}")
     String introduction,
-
-    @Size(min = 1, max = 5, message = "{size.validate.team.images}")
-    @NotNull(message = "{not.null.team.images")
-    List<MultipartFile> images,
 
     @Size(min = 1, max = 3, message = "{size.validate.team.members}")
     @NotNull(message = "{not.null.team.members")
