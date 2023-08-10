a4a4132842613fd7ca62fe30ef7cad86e39fdd48
KAispread <kai@igiuui-MacBookPro.local>
2023-08-10 14:02:50 +0900

    feat(persist): implement persist login

--- src/main/java/com/e2i/wemeet/dto/response/persist/PersistResponseDto.java
+++ src/main/java/com/e2i/wemeet/dto/response/persist/PersistResponseDto.java
@@ -1,5 +1,7 @@
 package com.e2i.wemeet.dto.response.persist;
 
+import com.e2i.wemeet.domain.member.persist.PersistData;
+import java.util.Objects;
 import lombok.Builder;
 
 @Builder
@@ -12,6 +14,39 @@ public record PersistResponseDto(
     boolean profileImageAuthenticated,
     boolean hasTeam
 ) {
+
+    public static PersistResponseDto of(PersistData persistData) {
+        final boolean emailAuthenticated = persistData.email() != null;
+        final boolean hasTeam = persistData.teamId() != null;
+
+        PersistResponseDtoBuilder builder = PersistResponseDto.builder()
+            .nickname(persistData.nickname())
+            .emailAuthenticated(emailAuthenticated)
+            .hasTeam(hasTeam);
+
+        if (Objects.isNull(persistData.profileImage())) {
+            return createResponseHasNoImage(builder);
+        }
+        return createResponse(persistData, builder);
+    }
+
+    private static PersistResponseDto createResponse(PersistData persistData, PersistResponseDtoBuilder builder) {
+        return builder
+            .hasMainProfileImage(persistData.profileImage().hasProfileImage())
+            .basicProfileImage(persistData.profileImage().getBasicUrl())
+            .lowProfileImage(persistData.profileImage().getLowUrl())
+            .profileImageAuthenticated(persistData.profileImage().getImageAuth())
+            .build();
+    }
+
+    private static PersistResponseDto createResponseHasNoImage(PersistResponseDtoBuilder builder) {
+        return builder
+            .hasMainProfileImage(false)
+            .profileImageAuthenticated(false)
+            .basicProfileImage(null)
+            .lowProfileImage(null)
+            .build();
+    }
 
     @Override
     public String toString() {
