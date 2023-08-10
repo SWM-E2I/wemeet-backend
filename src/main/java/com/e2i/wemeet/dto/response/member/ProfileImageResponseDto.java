b40047e652fea6bcc1bfad33c7617570d2cc409c
KAispread <kai@igiuui-MacBookPro.local>
2023-08-10 18:29:25 +0900

    fix(member): Add handling null from profile image instance

--- src/main/java/com/e2i/wemeet/dto/response/member/ProfileImageResponseDto.java
+++ src/main/java/com/e2i/wemeet/dto/response/member/ProfileImageResponseDto.java
@@ -1,6 +1,7 @@
 package com.e2i.wemeet.dto.response.member;
 
 import com.e2i.wemeet.domain.member.data.ProfileImage;
+import java.util.Objects;
 
 public record ProfileImageResponseDto(
     String basicUrl,
@@ -8,6 +9,10 @@ public record ProfileImageResponseDto(
 ) {
 
     public static ProfileImageResponseDto of(ProfileImage profileImage) {
+        if (Objects.isNull(profileImage)) {
+            return new ProfileImageResponseDto(null, null);
+        }
+
         return new ProfileImageResponseDto(
             profileImage.getBasicUrl(),
             profileImage.getLowUrl()
