a1d6e6929f0ad542f253e1d58395eb6028fb9eed
KAispread <kai@igiuui-MacBookPro.local>
2023-08-10 15:53:02 +0900

    test: add RequestEndPointChecker to AbstractIntegrationTest

--- src/test/java/com/e2i/wemeet/support/config/AbstractIntegrationTest.java
+++ src/test/java/com/e2i/wemeet/support/config/AbstractIntegrationTest.java
@@ -5,7 +5,10 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import com.e2i.wemeet.security.filter.AuthenticationExceptionFilter;
 import com.e2i.wemeet.security.filter.JwtAuthenticationFilter;
 import com.e2i.wemeet.security.filter.RefreshTokenProcessingFilter;
+import com.e2i.wemeet.security.filter.RequestEndPointCheckFilter;
 import com.e2i.wemeet.security.filter.SmsLoginProcessingFilter;
+import com.e2i.wemeet.security.handler.DispatcherServletEndPointChecker;
+import com.e2i.wemeet.security.handler.HttpRequestEndPointChecker;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -21,6 +24,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 /*
  * Integration Test Module
@@ -46,14 +50,26 @@ public abstract class AbstractIntegrationTest {
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    protected RequestEndPointCheckFilter requestEndPointCheckFilter;
+
+    protected HttpRequestEndPointChecker httpRequestEndPointChecker;
+
     protected MockMvc mvc;
 
     @BeforeEach
     void setup(WebApplicationContext context, RestDocumentationContextProvider restDocumentation) {
+        DispatcherServlet dispatcherServlet = MockMvcBuilders.webAppContextSetup(context)
+            .apply(documentationConfiguration(restDocumentation))
+            .build().getDispatcherServlet();
+
+        httpRequestEndPointChecker = new DispatcherServletEndPointChecker(dispatcherServlet);
+        requestEndPointCheckFilter = new RequestEndPointCheckFilter(httpRequestEndPointChecker);
+
         mvc = MockMvcBuilders.webAppContextSetup(context)
             .apply(documentationConfiguration(restDocumentation))
             .addFilter(authenticationExceptionFilter)
             .addFilters(refreshTokenProcessingFilter, SMSLoginProcessingFilter, jwtAuthenticationFilter)
+            .addFilter(requestEndPointCheckFilter)
             .build();
     }
 
