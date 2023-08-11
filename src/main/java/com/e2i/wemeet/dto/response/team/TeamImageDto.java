7f3f540015b51cb03a7bfeceb989e318466ae887
chaerim <qkrdbsk28@naver.com>
2023-08-12 01:36:49 +0900

    refactor(dto): delete builder annotation

--- src/main/java/com/e2i/wemeet/dto/response/team/TeamImageDto.java
+++ src/main/java/com/e2i/wemeet/dto/response/team/TeamImageDto.java
@@ -1,15 +1,10 @@
 package com.e2i.wemeet.dto.response.team;
 
-import lombok.Builder;
-
-@Builder
 public record TeamImageDto(
     String url
 ) {
 
     public static TeamImageDto of(String url) {
-        return TeamImageDto.builder()
-            .url(url)
-            .build();
+        return new TeamImageDto(url);
     }
 }
