9be28c5e305f0ccc58b403c786a576f9d6ad411d
chaerim <qkrdbsk28@naver.com>
2023-08-14 22:32:14 +0900

    fix: change test code error

--- src/main/java/com/e2i/wemeet/domain/member/MemberRepository.java
+++ src/main/java/com/e2i/wemeet/domain/member/MemberRepository.java
@@ -3,6 +3,7 @@ package com.e2i.wemeet.domain.member;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -11,5 +12,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
     @Query("select m from Member m join fetch m.collegeInfo.collegeCode where m.memberId = :memberId")
-    Optional<Member> findByIdFetchCode(Long memberId);
+    Optional<Member> findByIdFetchCode(@Param("memberId") Long memberId);
 }
