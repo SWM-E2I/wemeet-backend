5a42d0ae5c4f83baad4a5846d0f3c41326f54ca2
KAispread <kai@igiuui-MacBookPro.local>
2023-08-28 18:55:45 +0900

    feat(meeting): add get list methods that sent & received request to member's team

--- src/main/java/com/e2i/wemeet/service/meeting/MeetingListServiceImpl.java
+++ src/main/java/com/e2i/wemeet/service/meeting/MeetingListServiceImpl.java
@@ -1,12 +1,15 @@
 package com.e2i.wemeet.service.meeting;
 
 import com.e2i.wemeet.domain.meeting.MeetingRepository;
+import com.e2i.wemeet.domain.meeting.MeetingRequestRepository;
 import com.e2i.wemeet.dto.response.meeting.AcceptedMeetingResponseDto;
+import com.e2i.wemeet.dto.response.meeting.MeetingRequestResponseDto;
 import com.e2i.wemeet.dto.response.meeting.ReceivedMeetingResponseDto;
 import com.e2i.wemeet.dto.response.meeting.SentMeetingResponseDto;
 import com.e2i.wemeet.security.manager.IsManager;
 import com.e2i.wemeet.util.validator.CustomExpirationValidator;
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +25,7 @@ public class MeetingListServiceImpl implements MeetingListService {
     public static final int REQUEST_EXPIRE_DAY = 3;
 
     private final MeetingRepository meetingRepository;
+    private final MeetingRequestRepository meetingRequestRepository;
 
     /*
      * 미팅 목록 *
@@ -55,8 +59,17 @@ public class MeetingListServiceImpl implements MeetingListService {
      */
     @IsManager
     @Override
-    public List<SentMeetingResponseDto> getSentRequestList(final Long MemberId, final LocalDateTime findDateTime) {
-        return null;
+    public List<SentMeetingResponseDto> getSentRequestList(final Long memberId, final LocalDateTime findDateTime) {
+        List<SentMeetingResponseDto> sentRequestList = meetingRepository.findSentRequestList(memberId);
+        updateMeetingRequestIfNecessary(sentRequestList, findDateTime);
+
+        // 응답에 필요한 정보만 필터링
+        return sentRequestList.stream()
+            .filter(request -> request.isPending()
+                && !request.isDeleted()
+                && !CustomExpirationValidator.isExpiredOfDays(request.getRequestTime(), findDateTime, REQUEST_EXPIRE_DAY))
+            .sorted(Comparator.comparing(MeetingRequestResponseDto::getRequestTime).reversed())
+            .toList();
     }
 
     /*
@@ -69,8 +82,37 @@ public class MeetingListServiceImpl implements MeetingListService {
      */
     @IsManager
     @Override
-    public List<ReceivedMeetingResponseDto> getReceiveRequestList(final Long MemberId, final LocalDateTime findDateTime) {
-        return null;
+    public List<ReceivedMeetingResponseDto> getReceiveRequestList(final Long memberId, final LocalDateTime findDateTime) {
+        List<ReceivedMeetingResponseDto> receivedRequests = meetingRepository.findReceiveRequestList(memberId);
+        updateMeetingRequestIfNecessary(receivedRequests, findDateTime);
+
+        // 응답에 필요한 정보만 필터링
+        return receivedRequests.stream()
+            .filter(request -> request.isPending()
+                && !request.isDeleted()
+                && !CustomExpirationValidator.isExpiredOfDays(request.getRequestTime(), findDateTime, REQUEST_EXPIRE_DAY))
+            .sorted(Comparator.comparing(MeetingRequestResponseDto::getRequestTime).reversed())
+            .toList();
+    }
+
+
+    // 만료된 신청 & 삭제된 팀 MeetingRequest 상태 갱신 (acceptStatus = EXPIRED)
+    private void updateMeetingRequestIfNecessary(final List<? extends MeetingRequestResponseDto> requestList,
+        final LocalDateTime findDateTime) {
+        Stream<Long> expiredIds = requestList.stream()
+            .filter(MeetingRequestResponseDto::isPending)
+            .filter(request -> CustomExpirationValidator.isExpiredOfDays(request.getRequestTime(), findDateTime, REQUEST_EXPIRE_DAY))
+            .map(MeetingRequestResponseDto::getMeetingRequestId);
+
+        Stream<Long> deletedIds = requestList.stream()
+            .filter(MeetingRequestResponseDto::isPending)
+            .filter(MeetingRequestResponseDto::isDeleted)
+            .map(MeetingRequestResponseDto::getMeetingRequestId);
+
+        List<Long> updateList = Stream.concat(expiredIds, deletedIds)
+            .toList();
+
+        meetingRequestRepository.updateRequestToExpired(updateList);
     }
 
     // 만료된 미팅 & 삭제된 팀 Meeting 상태 갱신 (isOver = true)
