e25450c4b567c39af9eaa888f4c232467925eaa3
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2023-09-05 03:49:26 +0900

    refactor(team): add chatLink to team creation request data (#108)
    * refactor(team): add chatLink to team creation request data
    
    * refactor(team): use validation to chatLink data
    
    * test(team): modify chatLink data to TeamFixture
    
    * refactor(team): modify api response for get team data
    
    * test(team): change dto field name to team from data
    
    * test(team): modify TeamServiceTest

--- src/main/java/com/e2i/wemeet/dto/request/team/UpdateTeamRequestDto.java
+++ src/main/java/com/e2i/wemeet/dto/request/team/UpdateTeamRequestDto.java
@@ -3,6 +3,7 @@ package com.e2i.wemeet.dto.request.team;
 import com.e2i.wemeet.util.validator.bean.AdditionalActivityValid;
 import com.e2i.wemeet.util.validator.bean.DrinkRateValid;
 import com.e2i.wemeet.util.validator.bean.DrinkWithGameValid;
+import com.e2i.wemeet.util.validator.bean.KakaoOpenChatLinkValid;
 import com.e2i.wemeet.util.validator.bean.RegionValid;
 import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
@@ -34,6 +35,10 @@ public record UpdateTeamRequestDto(
     @NotNull
     @Length(min = 10, max = 150)
     String introduction,
+
+    @NotNull
+    @KakaoOpenChatLinkValid
+    String chatLink,
 
     @NotNull
     @Size(min = 1, max = 3)
--- src/main/java/com/e2i/wemeet/service/team/TeamServiceImpl.java
+++ src/main/java/com/e2i/wemeet/service/team/TeamServiceImpl.java
@@ -14,6 +14,7 @@ import com.e2i.wemeet.dto.request.team.CreateTeamRequestDto;
 import com.e2i.wemeet.dto.request.team.TeamMemberRequestDto;
 import com.e2i.wemeet.dto.request.team.UpdateTeamRequestDto;
 import com.e2i.wemeet.dto.response.team.MyTeamDetailResponseDto;
+import com.e2i.wemeet.dto.response.team.MyTeamResponseDto;
 import com.e2i.wemeet.exception.notfound.CodeNotFoundException;
 import com.e2i.wemeet.exception.notfound.MemberNotFoundException;
 import com.e2i.wemeet.service.aws.s3.S3Service;
@@ -82,15 +83,20 @@ public class TeamServiceImpl implements TeamService {
 
     @Transactional(readOnly = true)
     @Override
-    public MyTeamDetailResponseDto readTeam(Long memberId) {
+    public MyTeamResponseDto readTeam(Long memberId) {
         Member teamLeader = memberRepository.findById(memberId)
             .orElseThrow(MemberNotFoundException::new)
             .checkMemberValid();
 
+        if (!teamLeader.hasTeam()) {
+            return MyTeamResponseDto.of(teamLeader.hasTeam());
+        }
+
         List<TeamImageData> teamImages = teamRepository.findTeamWithTeamImages(
             teamLeader.getCurrentTeam().getTeamId());
 
-        return MyTeamDetailResponseDto.of(teamLeader.getCurrentTeam(), teamImages);
+        return MyTeamResponseDto.of(teamLeader.hasTeam(),
+            MyTeamDetailResponseDto.of(teamLeader.getCurrentTeam(), teamImages));
     }
 
     @Transactional
