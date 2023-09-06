c8b35ead9834ae414e48eb773f4a8c3b88f0ee96
chaerim <qkrdbsk28@naver.com>
2023-09-07 01:52:34 +0900

    feat(heart): add api to get sent&received heart

--- src/main/java/com/e2i/wemeet/service/heart/HeartService.java
+++ src/main/java/com/e2i/wemeet/service/heart/HeartService.java
@@ -1,8 +1,15 @@
 package com.e2i.wemeet.service.heart;
 
+import com.e2i.wemeet.dto.response.heart.ReceivedHeartResponseDto;
+import com.e2i.wemeet.dto.response.heart.SentHeartResponseDto;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface HeartService {
 
     void sendHeart(Long memberId, Long partnerTeamId, LocalDateTime requestTime);
+
+    SentHeartResponseDto getSentHeart(Long memberId, LocalDateTime requestTime);
+
+    List<ReceivedHeartResponseDto> getReceivedHeart(Long memberId, LocalDateTime requestTime);
 }
--- src/main/java/com/e2i/wemeet/service/heart/HeartServiceImpl.java
+++ src/main/java/com/e2i/wemeet/service/heart/HeartServiceImpl.java
@@ -6,12 +6,16 @@ import com.e2i.wemeet.domain.member.Member;
 import com.e2i.wemeet.domain.member.MemberRepository;
 import com.e2i.wemeet.domain.team.Team;
 import com.e2i.wemeet.domain.team.TeamRepository;
+import com.e2i.wemeet.dto.dsl.HeartTeamData;
+import com.e2i.wemeet.dto.response.heart.ReceivedHeartResponseDto;
+import com.e2i.wemeet.dto.response.heart.SentHeartResponseDto;
 import com.e2i.wemeet.exception.badrequest.HeartAlreadyExistsException;
 import com.e2i.wemeet.exception.badrequest.NotSendToOwnTeamException;
 import com.e2i.wemeet.exception.notfound.MemberNotFoundException;
 import com.e2i.wemeet.exception.notfound.TeamNotFoundException;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,7 +32,7 @@ public class HeartServiceImpl implements HeartService {
     @Transactional
     @Override
     public void sendHeart(Long memberId, Long partnerTeamId, LocalDateTime requestTime) {
-        Member member = memberRepository.findByIdFetchCode(memberId)
+        Member member = memberRepository.findById(memberId)
             .orElseThrow(MemberNotFoundException::new)
             .checkMemberValid();
 
@@ -39,7 +43,7 @@ public class HeartServiceImpl implements HeartService {
             throw new NotSendToOwnTeamException();
         }
 
-        // 이미 하트 보냈으면 끝
+        // 당일 하트를 모두 소진했는지 확인
         checkTodayHeart(team.getTeamId(), requestTime);
 
         Team partnerTeam = teamRepository.findById(partnerTeamId)
@@ -49,6 +53,35 @@ public class HeartServiceImpl implements HeartService {
             .team(team)
             .partnerTeam(partnerTeam)
             .build());
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public SentHeartResponseDto getSentHeart(Long memberId, LocalDateTime requestTime) {
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(MemberNotFoundException::new)
+            .checkMemberValid();
+
+        Team team = member.getCurrentTeam();
+        List<HeartTeamData> sentHeartTeam = teamRepository.findSentHeart(team.getTeamId(),
+            requestTime);
+
+        return SentHeartResponseDto.of(sentHeartTeam);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<ReceivedHeartResponseDto> getReceivedHeart(Long memberId,
+        LocalDateTime requestTime) {
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(MemberNotFoundException::new)
+            .checkMemberValid();
+
+        Team team = member.getCurrentTeam();
+        List<HeartTeamData> receivedHeartTeam = teamRepository.findReceivedHeart(
+            team.getTeamId(), requestTime);
+
+        return ReceivedHeartResponseDto.of(receivedHeartTeam);
     }
 
     private void checkTodayHeart(Long teamId, LocalDateTime requestTime) {
