0e663f3c880b15de1077452f136a4a6b0687e5b0
chaerim <qkrdbsk28@naver.com>
2023-09-10 20:02:45 +0900

    refactor(auth): add query to get member role

--- src/main/java/com/e2i/wemeet/service/admin/TokenAuthorizationService.java
+++ src/main/java/com/e2i/wemeet/service/admin/TokenAuthorizationService.java
@@ -1,10 +1,8 @@
 package com.e2i.wemeet.service.admin;
 
-import com.e2i.wemeet.domain.member.Member;
 import com.e2i.wemeet.domain.member.MemberRepository;
 import com.e2i.wemeet.domain.member.data.Role;
 import com.e2i.wemeet.exception.notfound.MemberNotFoundException;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,12 +15,12 @@ public class TokenAuthorizationService {
     private final MemberRepository memberRepository;
 
     public Role getMemberRoleByMemberId(Long memberId) {
-        Optional<Member> member = memberRepository.findById(memberId);
+        Role memberRole = memberRepository.findRoleByMemberId(memberId);
 
-        if (!member.isPresent()) {
+        if (memberRole == null) {
             throw new MemberNotFoundException();
         }
 
-        return member.get().getRole();
+        return memberRole;
     }
 }
