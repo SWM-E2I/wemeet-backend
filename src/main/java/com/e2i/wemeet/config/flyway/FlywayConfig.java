48844f6b1470e19f9074333ef9dcc279f8c7915f
chaerim <qkrdbsk28@naver.com>
2023-09-26 19:54:11 +0900

    fix(flyway): add FlywayBeanConfig to fix migration error

---
+++ src/main/java/com/e2i/wemeet/config/flyway/FlywayConfig.java
@@ -0,0 +1,18 @@
+package com.e2i.wemeet.config.flyway;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean
+    public FlywayMigrationStrategy cleanMigrateStrategy() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
+}
+
