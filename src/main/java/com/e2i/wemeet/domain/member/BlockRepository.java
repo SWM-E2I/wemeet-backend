e550b766298951ee4e215f69e0b2908908003dc0
KAispread <pain0121@naver.com>
2023-09-27 23:45:35 +0900

    feat(block): add find block member list query

--- src/main/java/com/e2i/wemeet/domain/member/BlockRepository.java
+++ src/main/java/com/e2i/wemeet/domain/member/BlockRepository.java
@@ -11,4 +11,17 @@ public interface BlockRepository extends JpaRepository<Block, Long> {
     @Query("select b from Block b join fetch b.blockMember where b.member.memberId = :memberId")
     List<Block> findAllByMemberId(@Param("memberId") Long memberId);
 
+    // 차단한 사용자 목록 조회 (ID)
+    @Query("select b.blockMember.memberId from Block b where b.member.memberId = :memberId")
+    List<Long> findBlockMemberIds(@Param("memberId") Long memberId);
+
+    // 차단한 사용자 목록 조회 (ID)
+    @Query("""
+            select b.blockMember.memberId
+            from Block b
+            join Team t on t.teamLeader.memberId = b.member.memberId
+            where t.teamId = :teamId
+        """)
+    List<Long> findBlockMemberIdsByTeamId(@Param("teamId") Long teamId);
+
 }
--- src/test/java/com/e2i/wemeet/domain/member/BlockRepositoryTest.java
+++ src/test/java/com/e2i/wemeet/domain/member/BlockRepositoryTest.java
@@ -1,11 +1,15 @@
 package com.e2i.wemeet.domain.member;
 
 import static com.e2i.wemeet.support.fixture.MemberFixture.KAI;
+import static com.e2i.wemeet.support.fixture.MemberFixture.RIM;
 import static com.e2i.wemeet.support.fixture.MemberFixture.SEYUN;
+import static com.e2i.wemeet.support.fixture.TeamFixture.HONGDAE_TEAM_1;
+import static com.e2i.wemeet.support.fixture.TeamMemberFixture.create_3_man;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
-import com.e2i.wemeet.support.fixture.MemberFixture;
+import com.e2i.wemeet.domain.team.Team;
+import com.e2i.wemeet.domain.team.TeamRepository;
 import com.e2i.wemeet.support.module.AbstractRepositoryUnitTest;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -20,13 +24,16 @@ class BlockRepositoryTest extends AbstractRepositoryUnitTest {
     @Autowired
     private BlockRepository blockRepository;
 
+    @Autowired
+    private TeamRepository teamRepository;
+
     @DisplayName("차단 목록을 조회할 수 있다.")
     @Test
     void findAllByMemberId() {
         // given
         Member kai = memberRepository.save(KAI.create(ANYANG_CODE));
         Member seyun = memberRepository.save(SEYUN.create(KOREA_CODE));
-        Member rim = memberRepository.save(MemberFixture.RIM.create(WOMANS_CODE));
+        Member rim = memberRepository.save(RIM.create(WOMANS_CODE));
         blockRepository.save(new Block(kai, seyun));
         blockRepository.save(new Block(kai, rim));
 
@@ -40,6 +47,44 @@ class BlockRepositoryTest extends AbstractRepositoryUnitTest {
                 tuple(kai, seyun),
                 tuple(kai, rim)
             );
+    }
+
+    @DisplayName("차단한 사용자 ID 목록을 조회할 수 있다.")
+    @Test
+    void findAllBlockMemberId() {
+        // given
+        Member kai = memberRepository.save(KAI.create(ANYANG_CODE));
+        Member seyun = memberRepository.save(SEYUN.create(KOREA_CODE));
+        Member rim = memberRepository.save(RIM.create(WOMANS_CODE));
+        blockRepository.save(new Block(kai, seyun));
+        blockRepository.save(new Block(kai, rim));
+
+        // when
+        List<Long> findBlockMemberIds = blockRepository.findBlockMemberIds(kai.getMemberId());
+
+        // then
+        assertThat(findBlockMemberIds).hasSize(2)
+            .containsExactly(seyun.getMemberId(), rim.getMemberId());
+    }
+
+    @DisplayName("내 팀 ID로 차단한 사용자 ID 목록을 조회할 수 있다.")
+    @Test
+    void findAllBlockMemberIdByTeamId() {
+        // given
+        Member kai = memberRepository.save(KAI.create(ANYANG_CODE));
+        Member seyun = memberRepository.save(SEYUN.create(KOREA_CODE));
+        Member rim = memberRepository.save(RIM.create(WOMANS_CODE));
+        blockRepository.save(new Block(kai, seyun));
+        blockRepository.save(new Block(kai, rim));
+
+        Team kaiTeam = teamRepository.save(HONGDAE_TEAM_1.create(kai, create_3_man()));
+
+        // when
+        List<Long> findBlockMemberIds = blockRepository.findBlockMemberIdsByTeamId(kaiTeam.getTeamId());
+
+        // then
+        assertThat(findBlockMemberIds).hasSize(2)
+            .containsExactly(seyun.getMemberId(), rim.getMemberId());
     }
 
 }
