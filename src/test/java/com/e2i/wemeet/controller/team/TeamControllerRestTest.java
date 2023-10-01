2fcdd0295082fb386c9bbf2b8dd377fb84f749d1
KAispread <pain0121@naver.com>
2023-10-01 23:35:07 +0900

    test(team): add team api REST test

---
+++ src/test/java/com/e2i/wemeet/controller/team/TeamControllerRestTest.java
@@ -0,0 +1,99 @@
+package com.e2i.wemeet.controller.team;
+
+import static com.e2i.wemeet.domain.member.data.Gender.MAN;
+import static com.e2i.wemeet.rest.support.RestAssuredRequestUtils.로그인된_상태로_DELETE_요청을_보낸다;
+import static com.e2i.wemeet.rest.support.RestAssuredRequestUtils.로그인된_상태로_GET_요청을_보낸다;
+import static com.e2i.wemeet.rest.support.RestAssuredRequestUtils.로그인된_상태로_이미지와_함께_PUT_요청을_보낸다;
+import static com.e2i.wemeet.support.fixture.RestAssuredFixture.째림;
+import static com.e2i.wemeet.support.fixture.RestAssuredFixture.카이;
+import static com.e2i.wemeet.support.fixture.TeamFixture.HONGDAE_TEAM_2;
+import static com.e2i.wemeet.support.fixture.TeamMemberFixture.createRequestDto_2_man;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.e2i.wemeet.domain.team.TeamRepository;
+import com.e2i.wemeet.dto.request.team.UpdateTeamRequestDto;
+import com.e2i.wemeet.rest.support.MultipartRequest;
+import com.e2i.wemeet.support.module.AbstractAcceptanceTest;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.io.IOException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class TeamControllerRestTest extends AbstractAcceptanceTest {
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @DisplayName("회원 가입을 하고 팀을 생성할 수 있다.")
+    @Test
+    void createTeam() throws IOException {
+        // given
+        final MultipartRequest<Object> imageRequest = createMultiPartRequest("images",
+            "classpath:/static/test_image/software maestro.png");
+
+        // when
+        ExtractableResponse<Response> response = 카이.회원가입하고_1번_팀을_생성한다(imageRequest);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(200);
+    }
+
+    @DisplayName("팀을 생성하고 수정할 수 있다")
+    @Test
+    void updateTeam() throws IOException {
+        // given
+        final MultipartRequest<Object> imageRequest = createMultiPartRequest("images",
+            "classpath:/static/test_image/software maestro.png");
+        UpdateTeamRequestDto updateTeamRequestDto = HONGDAE_TEAM_2.updateTeamRequestDto(createRequestDto_2_man());
+
+        String accessToken = 카이.회원가입을_한다();
+        카이.홍대1번_팀을_생성한다(accessToken, MAN, imageRequest);
+
+        // when
+        ExtractableResponse<Response> updateResponse = 로그인된_상태로_이미지와_함께_PUT_요청을_보낸다("/v1/team", accessToken,
+            imageRequest, updateTeamRequestDto);
+
+        // then
+        assertThat(updateResponse.statusCode()).isEqualTo(200);
+    }
+
+    @DisplayName("팀을 생성하고 삭제한 뒤, 다시 팀을 생성할 수 있다.")
+    @Test
+    void createAndDeleteAndCreate() throws IOException {
+        // given
+        final MultipartRequest<Object> imageRequest = createMultiPartRequest("images",
+            "classpath:/static/test_image/software maestro.png");
+
+        String accessToken = 카이.회원가입을_한다();
+        카이.홍대1번_팀을_생성한다(accessToken, MAN, imageRequest);
+        로그인된_상태로_DELETE_요청을_보낸다("/v1/team", accessToken);
+
+        // when
+        ExtractableResponse<Response> response = 카이.홍대2번_팀을_생성한다(accessToken, MAN, imageRequest);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(200);
+    }
+
+    @DisplayName("다른 팀을 조회할 수 있다.")
+        //@Test
+    void readOtherTeam() throws IOException {
+        // given
+        final MultipartRequest<Object> imageRequest = createMultiPartRequest("images",
+            "classpath:/static/test_image/software maestro.png");
+        String accessToken = 카이.회원가입을_한다();
+        카이.홍대1번_팀을_생성한다(accessToken, MAN, imageRequest);
+        째림.회원가입하고_1번_팀을_생성한다(imageRequest);
+
+        // when
+        ExtractableResponse<Response> response = 로그인된_상태로_GET_요청을_보낸다("/v1/team/2", accessToken);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(200);
+        assertThat(response.body().jsonPath().getString("data.leader.nickname"))
+            .isEqualTo(째림.getFixture().getNickname());
+    }
+
+}
