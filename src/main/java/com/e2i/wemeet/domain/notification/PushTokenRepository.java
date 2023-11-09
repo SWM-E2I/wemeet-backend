46cc64ade525badee046902b3d1fc12c38fd823e
KAispread <pain0121@naver.com>
2023-11-09 15:03:47 +0900

    feat: add find push token query

--- src/main/java/com/e2i/wemeet/domain/notification/PushTokenRepository.java
+++ src/main/java/com/e2i/wemeet/domain/notification/PushTokenRepository.java
@@ -1,7 +1,13 @@
 package com.e2i.wemeet.domain.notification;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PushTokenRepository extends JpaRepository<PushToken, String> {
+
+    @Query("select p from PushToken p where p.token = :token")
+    Optional<PushToken> findByToken(@Param("token") String token);
 
 }
