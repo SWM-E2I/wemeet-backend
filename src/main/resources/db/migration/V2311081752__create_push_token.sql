57a01374c43e8b4b7f4faa2888c8b1aa41336c7d
KAispread <pain0121@naver.com>
2023-11-09 15:05:38 +0900

    chore: add fields created_at, modified_at to PushToken Table

--- src/main/resources/db/migration/V2311081752__create_push_token.sql
+++ src/main/resources/db/migration/V2311081752__create_push_token.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS `push_token`
 (
     `token`             varchar(100)       NOT NULL,
-    `member_id`      bigint,
+    `member_id`         bigint,
+    `created_at`        datetime(6) NOT NULL,
+    `modified_at`       datetime(6) NOT NULL,
     PRIMARY KEY (`token`),
     FOREIGN KEY (`member_id`) REFERENCES member (`member_id`) ON DELETE CASCADE
     )
