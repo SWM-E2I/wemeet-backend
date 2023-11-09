b27c070c0cf833cbb5b8308c3ea0e2a3f3d39989
chaerim <qkrdbsk28@naver.com>
2023-11-10 02:20:07 +0900

    feat(notification): add scheduler for recommended push notifications

---
+++ src/main/java/com/e2i/wemeet/config/common/SchedulerConfig.java
@@ -0,0 +1,10 @@
+package com.e2i.wemeet.config.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+
+}
--- src/main/java/com/e2i/wemeet/domain/notification/PushTokenRepository.java
+++ src/main/java/com/e2i/wemeet/domain/notification/PushTokenRepository.java
@@ -1,7 +1,20 @@
 package com.e2i.wemeet.domain.notification;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PushTokenRepository extends JpaRepository<PushToken, String> {
 
+    @Query("select p.token from PushToken p where p.member.memberId is not null")
+    List<String> findAllMemberTokens();
+
+    @Query("""
+        select p.token
+        from PushToken p
+        join Member m on m.memberId = p.member.memberId
+        where p.member.memberId is not null
+        and m.role = 'ROLE_MEMBER'
+        """)
+    List<String> findTokensOfMemberWithoutTeam();
 }
