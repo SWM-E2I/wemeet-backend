fdc1278d41e5affcc107d5a0a58a8362f3472e65
KAispread <pain0121@naver.com>
2023-11-09 15:08:49 +0900

    test: test handler saving push token

--- src/test/java/com/e2i/wemeet/controller/TokenControllerTest.java
+++ src/test/java/com/e2i/wemeet/controller/TokenControllerTest.java
@@ -1,19 +1,23 @@
 package com.e2i.wemeet.controller;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.e2i.wemeet.dto.request.token.PushTokenRequestDto;
 import com.e2i.wemeet.dto.response.persist.PersistResponseDto;
 import com.e2i.wemeet.support.config.AbstractControllerUnitTest;
 import com.e2i.wemeet.support.config.WithCustomMockUser;
 import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -29,7 +33,7 @@ class TokenControllerTest extends AbstractControllerUnitTest {
 
         // when
         ResultActions perform = mockMvc.perform(
-            RestDocumentationRequestBuilders.get("/v1/auth/persist")
+            get("/v1/auth/persist")
                 .header("Authorization", "Bearer " + "token")
         );
 
@@ -71,6 +75,74 @@ class TokenControllerTest extends AbstractControllerUnitTest {
                             .description("팀의 존재 여부를 반환합니다.")
                     )
                 ));
+    }
+
+    @DisplayName("푸시 토큰을 저장할 수 있다.")
+    @Test
+    void savePushToken() throws Exception {
+        //given
+        given(accessTokenHandler.extractMemberId("token"))
+            .willReturn(Optional.of(1L));
+        PushTokenRequestDto request = new PushTokenRequestDto("token");
+
+        // when
+        ResultActions perform = mockMvc.perform(
+            post("/v1/push")
+                .contentType(APPLICATION_JSON)
+                .header("AccessToken", "Bearer " + "token")
+                .content(toJson(request))
+        );
+
+        // then
+        perform.andExpectAll(
+            status().isOk(),
+            jsonPath("$.status").value("SUCCESS"),
+            jsonPath("$.message").value("Push Token Save Success"),
+            jsonPath("$.data").isEmpty()
+        );
+
+        perform
+            .andDo(
+                MockMvcRestDocumentationWrapper.document("PUSH TOKEN 저장 요청",
+                    ResourceSnippetParameters.builder()
+                        .tag("PUSH TOKEN 저장")
+                        .summary("PUSH TOKEN을 저장합니다.")
+                        .description(
+                            """
+                                   PUSH 토큰을 저장합니다. AccessToken 헤더를 전달하지 않으면 memberId가 null 로 저장됩니다.
+                                   이미 DB 저장되어있는 토큰에 memberId를 업데이트하려면
+                                   body에 token, AccessToken 헤더에 해당 User에 AccessToken을 전달하면 됩니다.
+                                """),
+                    responseFields(
+                        fieldWithPath("status").type(JsonFieldType.STRING).description("응답 상태"),
+                        fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                        fieldWithPath("data").type(JsonFieldType.NULL).description("응답 데이터")
+                    )
+                ));
+    }
+
+    @DisplayName("액세스 토큰을 전달하지 않을 경우에도 푸시 토큰을 저장할 수 있다.")
+    @Test
+    void savePushToken_withNoAccessToken() throws Exception {
+        //given
+        given(accessTokenHandler.extractMemberId(""))
+            .willReturn(Optional.empty());
+        PushTokenRequestDto request = new PushTokenRequestDto("token");
+
+        // when
+        ResultActions perform = mockMvc.perform(
+            post("/v1/push")
+                .contentType(APPLICATION_JSON)
+                .content(toJson(request))
+        );
+
+        // then
+        perform.andExpectAll(
+            status().isOk(),
+            jsonPath("$.status").value("SUCCESS"),
+            jsonPath("$.message").value("Push Token Save Success"),
+            jsonPath("$.data").isEmpty()
+        );
     }
 
     private PersistResponseDto createPersistResponseDto() {
